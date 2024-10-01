0984fcc15d1cc580e7fd89575400c0169625550c
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-01 20:54:25 -0300

    explaning the main objectives

---
+++ getting-time-series-datasets/cleaning-datasets.R
@@ -0,0 +1,8 @@
+# CLEANING DATASETS
+
+### missing data;
+### frequency change of a time series (upsampling and downsampling);
+### data smoothing;
+### dealing with seasonality in data;
+### prevention of unintentional lookaheads;
+
