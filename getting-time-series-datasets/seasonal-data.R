8857026e83e5b091638909368d121f433ed84564
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-08 08:22:36 -0300

    seasonal data explanation

---
+++ getting-time-series-datasets/seasonal-data.R
@@ -0,0 +1,11 @@
+#____________________________________*****____________________________________#
+
+############################### Seasonal Data #################################
+
+### When it comes to data, seasonality is any type of recurring behavior in 
+### which the frequency is stable. It can occur at many different frequencies 
+### and at the same time. For example, human behavior is prone to having a daily 
+### seasonality (always having lunch at the same time), a weekly seasonality 
+### (going to the gym every Monday), and an annual seasonality (little traffic 
+### on New Year's Day). Physical periods also have seasonality, such as the 
+### period it takes for the Earth to revolve around the Sun.
