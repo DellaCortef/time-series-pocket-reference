1d0316ab8e59352c5b02d0c49ac599d2dfb5337e
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-09 19:29:46 -0300

    exploratory data analysis with known methods

--- exploratory-data-analysis-for-time-series/exploratory-data-analysis.R
+++ exploratory-data-analysis-for-time-series/exploratory-data-analysis.R
@@ -1,3 +1,21 @@
 # Exploratory Data Analysis
 
-### 
+### Known Methods
+
+#### Data exploration techniques typically used on **time series** datasets. 
+#### The process is the same as that used for data that are not time series 
+#### (for example, cross-sectional data). We would like to know the available 
+#### columns, their value ranges, and which logical units of measurement 
+#### work best.
+
+#### Questions to ask:
+  #### Are there strong correlations between the columns? Which?
+  #### What is the overall mean of a relevant variable? What is your variance?
+  #### What is the range of values we perceive? Do they vary by time period 
+  #### or other logical unit of analysis?
+  
+#### To answer the questions, we can use known techniques, such as:
+  #### plotting;
+  #### statistical synthesis;
+  #### histograms;
+  #### scatter plots;
