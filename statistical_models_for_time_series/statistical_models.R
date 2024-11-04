15b5e1e81aaca583ceec5748319a237257ebf48c
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-11-04 08:27:31 -0300

    Statistical Models for Time Series explanation

---
+++ statistical_models_for_time_series/statistical_models.R
@@ -0,0 +1,15 @@
+# Statistical Models for Time Series
+##### We will map some linear statistical models to **time series**.
+##### These models are related to linear regression, but represent 
+##### correlations between data points in the same **time series**, 
+##### unlike standard methods applied to cross-sectional data, where 
+##### Each data point is assumed to be independent of the others in the sample. 
+##### We will analyze the following models:
+  ##### - autoregressive models (AR), moving average models (MA) and models 
+  ##### autoregressive integrated moving average (ARIMA);
+  ##### - vector autoregression (VAR);
+  ##### - hierarchical models;
+##### Traditionally, these models have been the driving force in forecasting 
+##### **time series** and continue to be used in a wide range of 
+##### situations, from academic research to modeling in various fields 
+##### of acting.
