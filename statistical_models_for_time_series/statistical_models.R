a81d38859a96a8365f237dd5ab0c212ff4ade77f
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-11-06 07:37:36 -0300

    adding daily_demand_orders.csv file into datasets and plotting fiscal.sector.orders from thos file

--- statistical_models_for_time_series/statistical_models.R
+++ statistical_models_for_time_series/statistical_models.R
@@ -1,1 +1,12 @@
 # Statistical Models for Time Series
+
+# Setting the dataset path
+setwd('/Users/dellacorte/py-projects/data-science/time-series-pocket-reference/datasets/')
+
+# Load and structure the data
+demand <- read.csv("daily_demand_forecasting_orders.csv", sep=";")
+
+## plotting the data in chronological order
+plot(demand[, 'Fiscal.sector.orders'], type = 'b')
+acf(y2)
+pacf(y2)
