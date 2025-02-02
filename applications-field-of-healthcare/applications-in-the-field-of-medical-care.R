535f1d511a888599e7cef7c2d39c37fceec9be49
DellaCortef <felipe.dellacorte@outlook.com.br>
2025-02-01 21:46:38 -0300

    creating applications-in-the-field-of-medical-care.Rmkd file + DEL: removing applications-in-the-field-of-medical-care ipynb file + creating databases folder and download train.csv file from kaggle + creating r markdown

---
+++ applications-field-of-healthcare/applications-in-the-field-of-medical-care.R
@@ -0,0 +1,42 @@
+# Install packages
+install.packages("ISOweek")
+install.packages("data.table")
+
+# Load packages
+library(ISOweek)
+library(data.table)
+
+# Setting the dataset path
+setwd('/Users/dellacorte/py-projects/data-science/time-series-pocket-reference/datasets/')
+
+# Read the Flu CSV using fread
+flu <- fread("flu_train.csv")
+
+# Ensure flu is a data.table
+setDT(flu)  # Converts flu to data.table in-place
+
+# Replace non-numeric values in TauxGrippe before conversion
+flu[, TauxGrippe := as.character(TauxGrippe)]  # Ensure it's character
+flu[TauxGrippe %in% c("", "NA", "unknown", "-"), TauxGrippe := NA]  # Replace invalid entries with NA
+
+# Convert to numeric and store as flu.rate
+flu[, flu.rate := as.numeric(TauxGrippe)]
+
+# Replace NAs in flu.rate with 0 (if needed)
+flu[is.na(flu.rate), flu.rate := 0]
+
+# Print summary to check
+summary(flu$flu.rate)
+
+head(flu)
+
+# NA checking
+nrow(flu[is.na(flu.rate)]) / nrow(flu)
+
+unique(flu[is.na(flu.rate)]$region_name)
+
+flu[, year := as.numeric(substr(week, 1, 4))]
+flu[, wk   := as.numeric(substr(week, 5, 6))]
+
+flu[, date := ISOweek2date(paste0(substr(as.character(week), 1, 4), "-W", substr(as.character(week), 5, 6), "-1"))]
+
