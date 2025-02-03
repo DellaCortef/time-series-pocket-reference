70f99ebe1d200416d0dab9f7c7220ce8529bb0a8
DellaCortef <felipe.dellacorte@outlook.com.br>
2025-02-03 14:10:50 -0300

    arima-harmoni-regressors-flu-prediction image

--- applications-field-of-healthcare/applications-in-the-field-of-medical-care.R
+++ applications-field-of-healthcare/applications-in-the-field-of-medical-care.R
@@ -74,8 +74,8 @@ pacf(diff(diff(paris.flu$flu.rate, 52), 1), lag.max = 104)
 ## Arima adjustment
 ## Let's estimate 2 weeks 1st time
 first.fit.size <- 104
-h              <- 2
-n              <- nrow(paris.flu) - h - first.fit.size
+h <- 2
+n <- nrow(paris.flu) - h - first.fit.size
 
 ## Gets the default dimensions for fits that we will produce
 ## and related information such as coefficients
@@ -83,12 +83,12 @@ first.fit <- arima(paris.flu$flu.rate[1:first.fit.size], order = c(2, 1, 0),
                    seasonal = list(order = c(0, 1, 0), period = 52))
 first.order <- arimaorder(first.fit)
 
-## Pré-alocar espaço para armazenar nossas predições e coeficientes
+## Pre-allocate space to store our predictions and coefficients
 fit.preds <- array(0, dim = c(n, h))
 fit.coefs <- array(0, dim = c(n, length(first.fit$coef)))
 
-## Após ajuste inicial, avançamos o ajuste uma semana de cada vez, cada vez 
-## reajustando o modelo e salvando os novos coeficientes e a nova previsao
+## After initial adjustment, we advance the adjustment one week at a time, each time 
+## readjusting the model and saving the new coefficients and the new prediction
 for (i in (first.fit.size + 1):(nrow(paris.flu) - h)) {
   ## predict for an increasingly large window
   data.to.fit = paris.flu[1:i]
@@ -97,3 +97,56 @@ for (i in (first.fit.size + 1):(nrow(paris.flu) - h)) {
   fit.preds[i - first.fit.size, ] <- forecast(fit, h = 2)$mean
   fit.preds[i - first.fit.size, ] <- fit$coef
 }
+
+ylim <- range(paris.flu$flu.rate[300:400],
+              fit.preds[, h][(300-h):(400-h)])
+par(mfrow = c(1, 1))
+plot(paris.flu$date[300:400], paris.flu$flu.rate[300:400], 
+     ylim = ylim, cex = 0.8, 
+     main = "Actual and predicted flu with SARIMA (2, 1, 0), (0, 1, 0)", 
+     xlab = "Date", ylab = "Flu rate")
+lines(paris.flu$date[300:400], fit.preds[, h][(300-h):(400-h)], 
+      col = 2, type = "l", lty = 2, lwd = 2)
+
+## Pre-allocate vectors to maintain coefficients and adjustments
+fit.preds       <- array(0, dim = c(n, h))
+fit.coefs       <- array(0, dim = c(n, 100))
+
+## Exogenous regressors that are components of the Fourier series fitted to the data
+flu.ts          <- ts(log(paris.flu$flu.rate + 1) + 0.0002, frequency = 52)
+
+## Add small offsets as small values cause problems
+exog.regressors <- fourier(flu.ts, K = 2)
+exog.colnames   <- colnames(exog.regressors)
+
+## Adjust the model again each week with the expansion window
+## of training data
+for (i in (first.fit.size + 1):(nrow(paris.flu) - h)) {
+  data.to.fit       <- ts(flu.ts[1:i], frequency = 52)
+  exogs.for.fit     <- exog.regressors[1:i,]
+  exogs.for.predict <- exog.regressors[(i + 1):(i + h),]
+  
+  fit <- auto.arima(data.to.fit,
+                    xreg = exogs.for.fit,
+                    seasonal = FALSE)
+  
+  fit.preds[i - first.fit.size, ] <- forecast(fit, h = h,
+                                              xreg = exogs.for.predict)$mean
+  fit.coefs[i - first.fit.size, 1:length(fit$coef)] = fit$coef 
+}
+
+flu.ts = ts(log(paris.flu$flu.rate + 1) + 0.0001,
+            frequency = 52)
+fit <- auto.arima(data.to.fit, xreg = exogs.for.fit, seasonal = FALSE)
+
+fit.preds[i - first.fit.size, ] <- forecast(fit, h = h,
+                                            xreg = exogs.for.predict)$mean
+
+ylim = range(paris.flu$flu.rate)
+plot(paris.flu$date[300:400], paris.flu$flu.rate[300:400], 
+     ylim = ylim, cex = 0.8,
+     main = "Actual and predicted flu with ARIMA + harmonic regressors",
+     xlab = "Date", ylab = "Flu rate"
+     )
+lines(paris.flu$date[300:400], exp(fit.preds[, h][(300-h):(400-h)]),
+      lty = 2, lws = 2)
